1da9453d01e4d5c4c31518cd177717fc0087e055
Rodolfo Leal <27327749+rodolfoafl@users.noreply.github.com>
2024-02-22 17:31:28 -0300

    project setup

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.tsx",
+    "./src/components/**/*.tsx",
+    "./src/app/**/*.tsx",
+  ],
+  theme: {},
+  plugins: [],
+};
+export default config;
