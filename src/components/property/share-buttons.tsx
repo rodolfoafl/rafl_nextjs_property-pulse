50a135575a9375fbb381078b354e8625f3b5ff46
Rodolfo Leal <27327749+rodolfoafl@users.noreply.github.com>
2024-03-13 01:00:43 -0300

    added share buttons

--- src/components/property/share-buttons.tsx
+++ src/components/property/share-buttons.tsx
@@ -1,4 +1,15 @@
-import { Share2 } from 'lucide-react'
+'use client'
+
+import {
+  EmailIcon,
+  EmailShareButton,
+  FacebookIcon,
+  FacebookShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+  WhatsappIcon,
+  WhatsappShareButton,
+} from 'react-share'
 
 import { Property } from '@/data/types/property'
 
@@ -7,9 +18,48 @@ interface ShareButtonsProps {
 }
 
 export default function ShareButtons({ property }: ShareButtonsProps) {
+  const shareUrl = `${process.env.NEXT_PUBLIC_DOMAIN}/properties/${property._id}`
+
+  const formattedHashtag = property.type.replace(/\s/g, '')
+
   return (
-    <button className="flex w-full items-center justify-center rounded-full bg-orange-500 px-4 py-2 font-bold text-white hover:bg-orange-600">
-      <Share2 className="mr-2" size={20} /> Share Property
-    </button>
+    <>
+      <h3 className="pt-2 text-center text-xl font-bold">
+        Share this property:
+      </h3>
+
+      <div className="flex justify-center gap-3 pb-5">
+        <FacebookShareButton
+          url={shareUrl}
+          hashtag={`#${formattedHashtag}ForRent`}
+        >
+          <FacebookIcon size={40} round={true} />
+        </FacebookShareButton>
+
+        <TwitterShareButton
+          url={shareUrl}
+          title={property.name}
+          hashtags={[`${formattedHashtag}ForRent`]}
+        >
+          <TwitterIcon size={40} round={true} />
+        </TwitterShareButton>
+
+        <WhatsappShareButton
+          url={shareUrl}
+          title={property.name}
+          separator=":: "
+        >
+          <WhatsappIcon size={40} round={true} />
+        </WhatsappShareButton>
+
+        <EmailShareButton
+          url={shareUrl}
+          subject={property.name}
+          body={`Check out this property listing: `}
+        >
+          <EmailIcon size={40} round={true} />
+        </EmailShareButton>
+      </div>
+    </>
   )
 }
