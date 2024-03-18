77d9ece2300985dee583bd95e471b04689121db7
Rodolfo Leal <27327749+rodolfoafl@users.noreply.github.com>
2024-03-18 16:42:08 -0300

    added message api route

---
+++ src/app/api/messages/route.ts
@@ -0,0 +1,61 @@
+import connectDB from 'config/database'
+import { NextRequest, NextResponse } from 'next/server'
+import { DefaultUser, getServerSession } from 'next-auth'
+
+import { authOptions } from '@/lib/auth'
+
+import Message from '../../../data/models/Message'
+
+export async function POST(request: NextRequest) {
+  const session = await getServerSession(authOptions)
+  if (!session || !session.user) {
+    return new NextResponse(
+      JSON.stringify({ message: 'You must be logged in to send a message' }),
+      { status: 401 },
+    )
+  }
+
+  console.log('session::', session)
+
+  try {
+    const { name, email, phone, message, property, recipient } =
+      await request.json()
+
+    console.log('body::', name)
+
+    const sessionUser = session.user as DefaultUser
+    const { id: userId } = sessionUser
+
+    if (userId === recipient) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Can not send a message to yourself' }),
+        { status: 400 },
+      )
+    }
+
+    await connectDB()
+
+    const newMessage = new Message({
+      sender: userId,
+      recipient,
+      property,
+      name,
+      email,
+      phone,
+      body: message,
+    })
+
+    console.log('newMessage::', newMessage)
+
+    await newMessage.save()
+
+    return new NextResponse(
+      JSON.stringify({ message: 'Message sent successfuly' }),
+      {
+        status: 200,
+      },
+    )
+  } catch (error) {
+    return new NextResponse('Failed to send message', { status: 500 })
+  }
+}
